e4ca5833ae862561e6ff3145ae00ab6281ff4587
senane <abdosenane75@gmail.com>
2024-04-02 11:02:22 +0000

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <div className="flex items-center justify-center min-h-screen text-[100px]">
+                Nood 9aweed 3lia mn hna
+            </div>
+        </>
+    );
+}
+
+export default App;
