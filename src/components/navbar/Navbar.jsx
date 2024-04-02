14c42f467ebd3e3d2328be3da4e251d4e91a594c
senane <abdosenane75@gmail.com>
2024-04-02 12:09:23 +0000

    hello mocharaf

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <nav className="shadow shadow-black/10 py-4">
+            <div className="container mx-auto w-[85%]">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <Link to={"/"}>
+                            <h2 className="text-3xl text-primary">Youcare</h2>
+                        </Link>
+                    </div>
+                    <div className="flex items-center">
+                        <ul className="flex items-center gap-2">
+                            <li>
+                                <Link
+                                    to={"/home"}
+                                    className="text-secondary/90 hover:text-secondary"
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={"/home"}
+                                    className="text-secondary/90 hover:text-secondary"
+                                >
+                                    About
+                                </Link>
+                            </li>
+                        </ul>
+                        <div className="ml-7">
+                            <Link
+                                to={"login"}
+                                className="text-white bg-primary border-2 px-6 border-primary py-2 rounded-md mx-3"
+                            >
+                                <button>Login</button>
+                            </Link>
+                            <Link
+                                to={"registre"}
+                                className="text-primary border-2 border-primary px-6 py-2 rounded-md"
+                            >
+                                <button>Registre</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'primary' : '#2C1320',
+        'secondary' : '#5F4B66',
+        'third' : '#A7ADC6'
+      }
+    },
   },
   plugins: [],
 }
