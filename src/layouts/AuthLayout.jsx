059d6d3e869af6c54ffbfba4bd93963992d4bcb6
senane <abdosenane75@gmail.com>
2024-04-02 11:49:48 +0000

    hello mocharaf

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Navbar from '@components/navbar/Navbar'
 const AuthLayout = () => {
   return (
     <>
         <header>
-            Header
+            <Navbar/>
         </header>
         <main>
             <Outlet/>
