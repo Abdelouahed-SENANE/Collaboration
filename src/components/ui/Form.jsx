a5284b26525c1220db6f115e3b3b4e2672a8fa7d
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-04-05 01:02:51 +0000

    add update profile // TODO: add validation & apply to listing

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Input from "./Input";
 
-const Form = ({ handleSubmit, children }) => {
+const Form = ({ handleSubmit, children, classes }) => {
     return (
-        <div className="w-[600px] p-5 border border-slate-300 rounded-lg bg-white">
+        <div className={`w-[600px] p-5 border border-slate-300 rounded-lg bg-white ${classes}`}>
             <form onSubmit={handleSubmit}>{children}</form>
         </div>
     );
